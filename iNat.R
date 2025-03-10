3a88033282a43910654d677f40630c2777c20da1
Angela  van Winden <angelavanwinden@Angelas-MacBook-Pro-3.local>
2025-03-09 23:23:51 -0400

    iNat Project

--- iNat.R
+++ iNat.R
@@ -1,2 +1,3 @@
-iNat_Projects<-read.csv("iNat_Project.csv")
-overall<-data.frame("observations"=5876,"observers"=39,"species"=396)
+Complete_Project<-read.csv("observations-537390.csv")
+Summary_SubProjects<-read.csv("iNat_Projects.csv")
+Summary_Project<-data.frame("observations"=5876,"observers"=39,"species"=396)
