efe089f790ab101acad5930dd60031d050732b6d
Angela  van Winden <angelavanwinden@Angelas-MacBook-Pro-3.local>
2025-03-11 22:44:12 -0400

    3/11/2025

--- iNat.R
+++ iNat.R
@@ -1,12 +1,18 @@
-library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(dplyr)
 
+#SUMMARY
 #Complete data exported from iNaturlaist for all observations in project
 Complete_Project<-read.csv("observations-537390.csv")
+Complete_Project$day <- as.Date(Complete_Project$observed_on)
 #Breakdown (basic stats) of the sub-projects & umbrella project 
 Summary_SubProjects<-read.csv("iNat_Projects.csv")
+Summary_SubProjects$month<- as.Date(Summary_SubProjects$month_year, "%m/%d/%y")
 Summary_Project<-data.frame("observations"=5876,"observers"=39,"species"=396)
 #UCSBlooms project first observation was March 11,2019 last was September 29,2021
 
+#SPECIES
 #Top 8 species in UCSBlooms Project
 ProjectSpecies<-data.frame("Hawthorn"=419,"Pride"=413,"brittlebush"=301,
                            "daisy"=273,"poppy"=228,"blue lily"= 203,
@@ -19,8 +25,18 @@ GeoSpecies<-data.frame("pride"=468,"brittlebush"=458,"hawthorn"=435,"poppy"=298,
                        "daisy"=288,"blue lily"=209,"lemonade berry"=179,"natal lily"=132)
 #UCSBlooms project accounts for APPROX 65% of plant and 46% of all observations during the same time/area
 
+#OBSERVERS AND OBSERVATIONS
 #UCSBlooms Project - Vested Interest Observers Question
 PublicObservers<- Complete_Project %>% filter(user_login!="avanwinden",user_login!="taylorc",
-                                              user_login!="charliethrift",user_login!="michellelee",
-                                              user_login!="excarpobro")
+                                              user_login!="charliethrift",
+                                              user_login!="michellelee",user_login!="excarpobro")
 #Lab group made 3,331 observations. 5 people account for 56.69% of the observations in the project
+#historagm of all observations
+hist(Complete_Project$day, "month",freq = TRUE, xlab="Date of Observation", 
+     ylab="Number of Observations", main = "UCSBlooms Observations")
+#graph seperating type of observation 
+Summary_SubProjects %>% ggplot(aes(x=month,y=observations,fill=bioblitz))+ 
+  geom_col(position = "stack")
+
+
+
