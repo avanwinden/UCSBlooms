9c0442b06c12a377bc5d4d55da2fcdf3317d71a2
Angela  van Winden <angelavanwinden@Angelas-MacBook-Pro-3.local>
2025-03-12 20:59:09 -0400

    iNat Update

--- iNat.R
+++ iNat.R
@@ -6,9 +6,11 @@ library(dplyr)
 #Complete data exported from iNaturlaist for all observations in project
 Complete_Project<-read.csv("observations-537390.csv")
 Complete_Project$day <- as.Date(Complete_Project$observed_on)
+year_Project<-subset(Complete_Project,day <= "2020-04-01")
 #Breakdown (basic stats) of the sub-projects & umbrella project 
 Summary_SubProjects<-read.csv("iNat_Projects.csv")
 Summary_SubProjects$month<- as.Date(Summary_SubProjects$month_year, "%m/%d/%y") #anytime within month assigned to first of month
+year_SubProjects<-subset(Summary_SubProjects, month <="2020-04-01")
 Summary_Project<-data.frame("observations"=5876,"observers"=39,"species"=396)
 
 
@@ -29,13 +31,19 @@ GeoSpecies<-data.frame("pride"=468,"brittlebush"=458,"hawthorn"=435,"poppy"=298,
 PublicObservers<- Complete_Project %>% filter(user_login!="avanwinden",user_login!="taylorc",
                                               user_login!="charliethrift",
                                               user_login!="michellelee",user_login!="excarpobro")
+year_PublicObservers<-year_Project%>% filter(user_login!="avanwinden",user_login!="taylorc",
+                                             user_login!="charliethrift",
+                                             user_login!="michellelee",user_login!="excarpobro")
 #historagm of all observations
 hist(Complete_Project$day, "month",freq = TRUE, xlab="Date of Observation", 
      ylab="Number of Observations", main = "UCSBlooms Observations")
+hist(year_Project$day, "month",freq = TRUE, xlab="Date of Observation", 
+     ylab="Number of Observations", main = "UCSBlooms Observations")
 #graph separating type of observation
 Summary_SubProjects %>% ggplot(aes(x=month,y=observations,fill=bioblitz))+ 
+  geom_col(position = "stack") 
+year_SubProjects %>% ggplot(aes(x=month,y=observations,fill=bioblitz))+ 
   geom_col(position = "stack")
-
 
 
 
