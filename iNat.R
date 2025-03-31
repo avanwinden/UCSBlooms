292eccff44f17d46eceb9e6f44f00c170bab3d6d
Angela  van Winden <angelavanwinden@Angelas-MacBook-Pro-3.local>
2025-03-30 20:55:41 -0400

    3/30/2025

--- iNat.R
+++ iNat.R
@@ -104,8 +104,3 @@ t<-chisq.test(percent) #i dont think this is correct
 ## any effects of the pandemic to explore?
 
 
-## FOCAL SPECIES INFLUENCE 
-#look at histogram of observations of species  taken to phenostage vs historgram of all obserservation 
-
-
-
