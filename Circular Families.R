85042ab4c0324f96285debc99289de13691404f5
Angela  van Winden <angelavanwinden@Angelas-MacBook-Pro-3.local>
2025-04-14 19:58:35 -0400

    4/14/25

--- Circular Families.R
+++
@@ -1,2 +0,0 @@
-# Question: Do the families behave differently from each other?
-# do we need to pair or can we group the families as one? look at literature review and see how comparisons are presented
